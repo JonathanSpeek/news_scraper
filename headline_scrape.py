e7355b7706d28ff2c0b4f98781a33c2a9a86b78c
Jonathan Speek <jonspeek@gmail.com>
2017-12-08 10:53:42 -0700

    fixing some encoding issues in a hacky way, styling a bit

--- headline_scrape.py
+++ headline_scrape.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
 import requests
-import os, errno
+import os
+import errno
 from datetime import date
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from time import sleep
+import re
 
 ua = {"User-Agent":"Mozilla/5.0"}
 today = date.today()
 day = today.strftime('%d')
 month = today.strftime('%m')
 year = today.year
+
 
 def get_nyt_story(url):
     page = requests.get(url, headers=ua)
@@ -18,14 +20,17 @@ def get_nyt_story(url):
     headline = soup.select('h1.headline')[0].text
     formatted_headline = headline.replace(' ', '')[0:15]
     try:
-        os.makedirs('nyt/{year}-{month}-{day}'.format(month=month,day=day,year=year))
+        os.makedirs('nyt/{year}-{month}-{day}'.format(month=month, day=day, year=year))
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    with open('nyt/{year}-{month}-{day}/{headline}.html'.format(headline=formatted_headline,month=month,day=day,year=year), 'w') as f:
+    with open('nyt/{year}-{month}-{day}/{headline}.html'.format(headline=formatted_headline, month=month, day=day, year=year), 'w') as f:
+        f.write('<!doctype html><html lang="en"><head><meta charset="utf-8"><link href="https://fonts.googleapis.com/css?family=Playfair+Display:700i" rel="stylesheet"><style>body{text-align:center;margin:2% 20%;}h1{font-family: NYTImperial, "Playfair Display", serif;font-style: italic;font-size: 2.125rem;line-height: 2.375rem;font-weight: 700;}p{text-align: left;line-height: 1.625rem;font-weight: 400;font-style: normal;font-family: georgia;font-size: 1.0625rem;}</style></head><body>')
         f.write('<h1>' + headline.strip() + '</h1>' + '\n\n')
         for section in body:
             f.write('<p>' + section.text.strip() + '</p>' + '\n')
+        f.write('</body></html>')
+
 
 def get_wp_story(url):
     page = requests.get(url, headers=ua)
@@ -39,13 +44,37 @@ def get_wp_story(url):
         if e.errno != errno.EEXIST:
             raise
     with open('wp/{year}-{month}-{day}/{headline}.html'.format(headline=formatted_headline,month=month,day=day,year=year), 'w') as f:
+        f.write('<!doctype html><html lang="en"><head><meta charset="utf-8"><link href="https://fonts.googleapis.com/css?family=Playfair+Display:700i" rel="stylesheet"><style>body{text-align:center;margin:2% 20%;}h1{font-family: NYTImperial, "Playfair Display", serif;font-style: italic;font-size: 2.125rem;line-height: 2.375rem;font-weight: 700;}p{text-align: left;line-height: 1.625rem;font-weight: 400;font-style: normal;font-family: georgia;font-size: 1.0625rem;}</style></head><body>')
         f.write('<h1>' + headline.strip() + '</h1>' + '\n\n')
         for section in body:
             if len(section.text) > 2:
-                f.write('<p>' + section.text.strip() + '</p>' + '\n')
-    
+                new_text = re.sub('\r?’', "'", section.text)
+                new_text = re.sub('\r?‘', "'", new_text)
+                new_text = re.sub('\r?”', '"', new_text)
+                new_text = re.sub('\r?“', '"', new_text)
+                f.write('<p>' + new_text.strip() + '</p>' + '\n')
+        f.write('</body></html>')
+
 
 def get_headlines(site):
+    """
+    Example usage:
+        >>> get_headlines('wp')
+
+    Expected return:
+        new directories created 'wp' >> '[today's date] >> [article-title].html
+
+    Args:
+        'wp', 'nyt', 'politico'
+
+    Issues:
+        NY Times can be quirky, Politico currently just grabs links to the front-page articles.
+
+    Todo:
+        Still need to finish Politico implementation, work on folder structure to create a static site to access all
+        articles, and refactor code significantly.
+    """
+
     if site is 'wp':
         url = "http://www.washingtonpost.com"
         page = requests.get(url, headers=ua)
@@ -93,8 +122,8 @@ def get_headlines(site):
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
-        with open('politico/{month}-{day}-{year}.txt'.format(month=month,day=day,year=year), 'a') as f:
-            f.write('--- Politico Headlines ---\n')
+        with open('politico/{month}-{day}-{year}.html'.format(month=month, day=day, year=year), 'a') as f:
+            f.write('Politico Headlines for {month}-{day}-{year}\n'.format(month=month, day=day, year=year))
             for link in links:
-                if 'www.politico.com/story/{year}/{month}/{day}/'.format(year=year,month=month,day=day) in link.get('href'):
-                    f.write('{href}\n'.format(href=link.get('href')))
+                if 'www.politico.com/story/{year}/{month}/{day}/'.format(year=year, month=month, day=day) in link.get('href'):
+                    f.write('<a href="{href}">{href}</a>\n'.format(href=link.get('href')))
